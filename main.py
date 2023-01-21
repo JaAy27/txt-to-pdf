c0675e5e9778785a1f1317d5df1ddf5d176f1e0d
JaAy27 <jnax27@hotmail.com>
2023-01-20 23:02:13 -0500

    Add text file content

--- main.py
+++ main.py
@@ -5,21 +5,29 @@ from pathlib import Path
 
 # Create a list of text filepaths
 filepaths = glob.glob("Files/*.txt")
+
 # Create one PDF file
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 # Go through each text file
 for filepath in filepaths:
-    #Add a page to the PDF document for each text file
-    #df = pd.read_csv(filepath) # this was unnecessary
 
     # Add page and page content, create PDF
     pdf.add_page()
     filename = Path(filepath).stem
 
+
     # Add the name to the PDF
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"{filename.capitalize()}")
+    pdf.cell(w=50, h=8, txt=f"{filename.capitalize()}", ln=1)
+
+    # Get the content on each text file
+    with open(filepath, "r") as file:
+        content = file.read()
+
+    # Add the text file content to the PDF
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 # Produce the PDF
 pdf.output("Output.pdf")
